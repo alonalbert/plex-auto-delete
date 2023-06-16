aeaa98141a6b48a3ed0fedb3d2b9a4f32103c081
Alon Albert <aalbert@google.com>
2023-06-16 14:36:57 -0700

    Plurals

--- app/src/main/kotlin/com/alonalbert/pad/app/ui/userlist/UserListScreen.kt
+++ app/src/main/kotlin/com/alonalbert/pad/app/ui/userlist/UserListScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -105,8 +106,8 @@ private fun UserCard(user: User, onUserClick: (User) -> Unit) {
 
 @Composable
 private fun User.displayType() = when (type) {
-    EXCLUDE -> stringResource(R.string.excludes_shows, shows.size)
-    INCLUDE -> stringResource(R.string.includes_shows, shows.size)
+    EXCLUDE -> pluralStringResource(id = R.plurals.excludes_shows, count = shows.size)
+    INCLUDE -> pluralStringResource(id = R.plurals.includes_shows, count = shows.size)
 }
 
 // Previews
