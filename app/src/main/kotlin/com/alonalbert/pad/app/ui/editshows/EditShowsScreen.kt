6e10f168e90d6fec0095ee555f84431258ae7f0d
Alon Albert <aalbert@google.com>
2023-06-17 09:05:28 -0700

    focus

--- app/src/main/kotlin/com/alonalbert/pad/app/ui/editshows/EditShowsScreen.kt
+++ app/src/main/kotlin/com/alonalbert/pad/app/ui/editshows/EditShowsScreen.kt
@@ -22,11 +22,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -115,6 +119,7 @@ private fun FilterField(filter: ShowFilter, onFilterChange: (ShowFilter) -> Unit
         true -> Icons.Filled.CheckCircle to "Show selected only"
         false -> Icons.Filled.CheckCircleOutline to "Show all"
     }
+    val focusRequester = remember { FocusRequester() }
 
     OutlinedTextField(
         value = filter.query,
@@ -129,9 +134,13 @@ private fun FilterField(filter: ShowFilter, onFilterChange: (ShowFilter) -> Unit
 
         },
         modifier = Modifier
+            .focusRequester(focusRequester)
             .fillMaxWidth()
             .padding(8.dp)
     )
+    LaunchedEffect(key1 = Unit) {
+        focusRequester.requestFocus()
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
