8af890d15529b3af402dfb72f606f7e726af029d
Alon Albert <aalbert@google.com>
2023-06-17 10:35:45 -0700

    IME Insets

--- app/src/main/kotlin/com/alonalbert/pad/app/ui/components/PadScaffold.kt
+++ app/src/main/kotlin/com/alonalbert/pad/app/ui/components/PadScaffold.kt
@@ -1,6 +1,11 @@
 package com.alonalbert.pad.app.ui.components
 
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -13,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.alonalbert.pad.app.ui.PadViewModel
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun PadScaffold(
     viewModel: PadViewModel,
@@ -24,17 +30,24 @@ fun PadScaffold(
     val message by viewModel.messageState.collectAsStateWithLifecycle()
     val isLoading by viewModel.isLoadingState.collectAsStateWithLifecycle()
 
+
     Scaffold(
         snackbarHost = { SnackbarHost(snackbarHostState) },
         floatingActionButton = floatingActionButton,
         modifier = modifier
             .fillMaxSize()
-            .padding(8.dp),
+            .padding(8.dp)
+            .imePadding(),
     ) { padding ->
+        val isImeVisible = WindowInsets.isImeVisible
+        val contentPadding = remember(isImeVisible) {
+            if (isImeVisible) PaddingValues(top = padding.calculateTopPadding()) else padding
+        }
         LoadingContent(
             isLoading = isLoading,
             onRefresh = viewModel::refresh,
-            modifier = modifier.padding(padding)
+            modifier = modifier
+                .padding(contentPadding)
         ) {
             content()
         }
