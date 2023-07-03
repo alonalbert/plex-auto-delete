ba4fc520a504a89927c7c5d6fdd655098fb498c1
Alon Albert <aalbert@google.com>
2023-07-03 10:24:37 -0700

    Deploy script

---
+++ deploy-server.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+cd "$(dirname "$0")"
+
+./gradlew :server:assemble || exit
+
+cd server
+
+docker build -t alonalbert/padserver . || exit
+
+docker-compose down || exit
+docker-compose up -d --remove-orphans
